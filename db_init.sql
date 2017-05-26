e6b7fd7ce7b9dfcfc652fb758795e228e6319d61
kamil <kamil.pilawski@gmail.com>
2017-05-26 18:25:58 +0200

    Zmiana skryptu SQL - user o id 3 to teraz EMAIL

--- db_init.sql
+++ db_init.sql
@@ -217,7 +217,7 @@ DEFAULT CHARACTER SET = utf8;
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'admin','admin'),(2,'mod','mod'),(3,'user','user');
+INSERT INTO `user` VALUES (1,'admin','admin'),(2,'mod','mod'),(3,'user@email.com','user');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
