22a3b12f226ab5be648859719c1727a12f9db70f
kamil <kamil.pilawski@gmail.com>
2017-06-15 12:04:37 +0200

    Dodanie nowego, wlasciwego endpointu dla sprawdzania uprawnien/bycia zalogowanym

--- src/main/kotlin/us/tla/controller/RoleController.kt
+++ src/main/kotlin/us/tla/controller/RoleController.kt
@@ -4,6 +4,8 @@ import mu.KLogging
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -30,6 +32,7 @@ class RoleController {
     @GetMapping("list")
     fun listRoles(principal: Principal): ResponseEntity<List<Role>> {
         logger.info("list roles")
+
         val user = userRepo.findByEmail(principal.name).get()
 
         val roles = roleRepo.findUserRoles(user.id)
@@ -37,5 +40,12 @@ class RoleController {
                 roles.orElse(listOf(Role())),
                 if (roles.isPresent) HttpStatus.OK else HttpStatus.NOT_FOUND
         )
+    }
+
+    @GetMapping("auth") //todo front w ten sposob powinien sprawdzac czy jest zalogowany
+    fun listRolesOther(auth: Authentication): ResponseEntity<MutableCollection<out GrantedAuthority>> {
+        logger.info("list auth")
+
+        return ResponseEntity(auth.authorities, HttpStatus.OK)
     }
 }
